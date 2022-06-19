bb2bb98ff335e6461e48adbbec4e5f92322360f4
mohammedek <mohammedek159@gmail.com>
2022-06-19 07:01:31 +0530

    v.1.01

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,22 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'FLutter_clone',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
       home: Scaffold(
+        extendBodyBehindAppBar: true,
         appBar: AppBar(
           title: Image.asset('assets/images/logo_title1.png',height: 90.0,width: 150,),
+          // actions: [
+          //   IconButton(onPressed: (){}, icon: Icon(Icons.search),
+          //   )
+          // ],
           backgroundColor: Colors.transparent,elevation: 0,
             ),
-        endDrawer: Drawer(
+         endDrawer: Drawer(
           child: ListView(
             padding: const EdgeInsets.all(0.0),
             children: const [
@@ -30,8 +40,14 @@ class _MyAppState extends State<MyApp> {
               ),ListTile(
                 title: Text('want to make it dropdown'),
               ),
-
             ],
+          ),
+        ),
+        body: Center(
+          child: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [Colors.purple,Colors.blueAccent,Colors.lightBlueAccent])
+            ),
           ),
         ),
         ),
